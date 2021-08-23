4816e1efefc562e955f12bb29c28a3cabafd1596
katsuki-hub <44katsu26@gmail.com>
2021-08-24 00:08:27 +0900

    グローバルナビ完成

--- scripts/movement.js
+++ scripts/movement.js
@@ -1,10 +1,19 @@
 /*=====================
 メニュースライド
 =====================*/
-$(document).ready(function () {
-  $(".btn").on("click", function () {
-    $("#boxmenu").slideToggle();
-  });
+$("#open_btn").click(function () {
+  $("#boxmenu").toggleClass('panelactive');
+});
+
+$("#boxmenu a").click(function () {
+  $("#boxmenu").removeClass('panelactive');
+});
+
+$('#boxmenu a').click(function () {
+  var elmHash = $(this).attr('href'); //hrefの内容を取得
+  var pos = Math.round($(elmHash).offset().top - 60); //headerの高さを引く
+  $('body,html').animate({ scrollTop: pos }, 1000);//※数値が大きいほどゆっくりスクロール
+  return false;
 });
 
 /*=====================
